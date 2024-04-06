3ae9b741f21b082b0f24f6a350db1e97fa130b9c
Jack Parsons <jackparse14@hotmail.co.uk>
2024-04-06 13:02:48 +0100

    script.js updates

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const API_URL = "https://api.openai.com/v1/chat/completions";
 let API_KEY = "";
-fetchConfig();
-function fetchConfig(){
+function fetchKey(){
     fetch("./config.json")
         .then((response) => response.json())
         .then((data) => API_KEY = data.OPEN_AI_KEY)
@@ -12,18 +11,28 @@ function fetchConfig(){
 function callApi(){
     const requestBody = {
         model: "gpt-3.5-turbo",
-        prompt: "Enter your prompt here",
+        messages: [
+            {
+                role: "user",
+                content: "Tell me about OpenAI"
+            }
+        ],
         max_tokens: 50
     }
     fetch(API_URL,{   
         method: "POST",
-        
         headers: {
             Authorization: `Bearer ${API_KEY}`,
             'content-type': 'application/json',
         },
-        body: JSON.stringify({requestBody})
+        body: JSON.stringify(requestBody)
     })
-    .then((response) => console.log('This is the response: ',response.json()))
-    .then((data) => console.log("This is the data: " ,data));
+    .then((response) => {
+        if (!response.ok){
+            throw new Error('Network response not ok: ' + response.statusText);
+        }
+        return response.json();
+    })
+    .catch((error) => console.log('Error making api request: ', error ));
 }
+fetchKey();
