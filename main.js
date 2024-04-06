6689f404e5c0f84febe65a798c7e7b773c134381
Todd <toddtaylor189@gmail.com>
2024-04-06 11:56:39 +0100

    submission now functioning

---
+++ main.js
@@ -0,0 +1,9 @@
+function formSubmit() {
+    const userInput = document.getElementById('input');
+    console.log('stringSubmitted');
+}
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById('submit').addEventListener('click', formSubmit);
+})
