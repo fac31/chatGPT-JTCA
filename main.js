82cf318ef339f7324a3f15f7cc20feefe109d72d
Todd <toddtaylor189@gmail.com>
2024-05-02 13:03:53 +0100

    partial code for history of q and a

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import { callApi } from "./api-requests.js";
 const answer = document.getElementById("answer");
 const question = document.getElementById("question");
+let questionList = document.querySelector(".questionList")
 
 async function formSubmit() {
   console.log("stringSubmitted");
@@ -10,6 +11,11 @@ async function formSubmit() {
 
   callApi(userInput)
   .then(response => {
+    let listItem = document.createElement("li")
+    listItem.classList = 'historicItem'
+
+    let newQuestion = document.createElement("p")
+    listItem.classList
       answer.innerHTML = response.choices[0].message.content;
   });
   userInput = "";
@@ -18,3 +24,13 @@ async function formSubmit() {
 document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("submit").addEventListener("click", formSubmit);
 });
+
+//Keeps the history of previous questions and answers
+  /*Empty list in HTML
+Within list the new question and answer is added 
+All done from main File
+Local storage is incorporated for memory*/
+
+
+
+
