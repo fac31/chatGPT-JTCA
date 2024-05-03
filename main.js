6d6e1017d6aaed5e6547173099e82fedb79153d7
Todd <toddtaylor189@gmail.com>
2024-05-03 09:56:23 +0100

    adding first test to testing library

--- main.js
+++ main.js
@@ -25,12 +25,53 @@ document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("submit").addEventListener("click", formSubmit);
 });
 
-//Keeps the history of previous questions and answers
-  /*Empty list in HTML
-Within list the new question and answer is added 
-All done from main File
-Local storage is incorporated for memory*/
+//************ testing library ***************//
 
+/* This is the most simple JavaScript test. I am going to create a command line function so that we can use it. 
+It calls the command function of Ping and then checks that the result is what you expect (Pong).*/
 
+function equal(pong, actual) {
+  if (actual === pong) {
+    console.info(`Pass: Expected ${pong} and received ${actual}`);
+  } else {
+    console.error(`Fail: Expected ${pong} but received ${actual}`);
+  }
+}
 
+client.once("ready", async () => {
+
+  if (guild) { // Not sure on what the guild equivalent is for our bot - Would you be able to help me on this?
+    await guild.commands.set([
+      {
+        name: "fortune-cookie",
+        description: "This is your fortune",
+        options: [
+          {
+            type: 3, // This corresponds to STRING type
+            name: "text",
+            description: "Your input text", // Description is required for each option
+            required: false, // This field is not required
+          },
+        ],
+      },
+    ]);
+    console.log("/commands have been registered");
+  }
+});
+
+// generate interaction with command
+client.on("interactionCreate", async (interaction) => {
+  if (!interaction.isCommand()) return;
+
+  const { commandName } = interaction;
+
+  if (commandName === "fortune-cookie") {
+    const fortune = await getFortuneCookie();
+    await interaction.reply(fortune);
+  }
+});
+
+//could also do a testing method to see if the AI's response is longer than the amount of characters it is allowed to reply back with?!
+
+//**********
 
